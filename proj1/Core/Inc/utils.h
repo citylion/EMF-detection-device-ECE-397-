5ecd56f0582828d72396dc08c7366d9ba387945c
citylion <citylionone@gmail.com>
2025-03-07 11:18:34 -0600

    Initial working demo

---
+++ proj1/Core/Inc/utils.h
@@ -0,0 +1,13 @@
+/*
+ * utils.h
+ *
+ *  Created on: Feb 28, 2025
+ *      Author: citylion
+ */
+
+#ifndef INC_UTILS_H_
+#define INC_UTILS_H_
+
+
+
+#endif /* INC_UTILS_H_ */
---
+++ proj1/Core/Src/utils.c
@@ -0,0 +1,11 @@
+/*
+ * utils.c
+ *
+ *  Created on: Feb 28, 2025
+ *      Author: citylion
+ */
+#include "main.h"
+#include <stdio.h>
+
+#include "stm32g4xx_hal.h"
+
