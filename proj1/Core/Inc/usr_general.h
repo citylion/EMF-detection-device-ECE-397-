67bbe404051f9854d62e54c50620c35c50dff2da
dmedi2 <124078838+dmedi2@users.noreply.github.com>
2025-04-10 13:53:13 -0500

    upload files for previous commit

---
+++ proj1/Core/Inc/usr_general.h
@@ -0,0 +1,18 @@
+#ifndef USR_GENERAL_H_
+#define USR_GENERAL_H_
+
+#define bool _Bool
+#define _io static
+
+#define true 1
+#define false 0
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include "stm32g4xx_hal.h"
+#include "l86_gnss_parser.h"
+
+#endif
